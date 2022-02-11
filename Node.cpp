2be9bb3bb1e7ed9afb72644e8b00bc6ea295590b
Semesus <devinzc@gmail.com>
2022-02-10 16:45:27 -0800

    v1, Node class implemented

--- Node.cpp
+++ Node.cpp
@@ -3,3 +3,92 @@
 //
 
 #include "Node.h"
+
+Node::Node() {}
+
+Node::Node(const std::string &key) : smKey_{key}, left_{nullptr}, mid_{nullptr}, right_{nullptr} {}
+
+Node::Node(const std::string &key, std::shared_ptr<Node> leftPtr, std::shared_ptr<Node> midPtr,
+           std::shared_ptr<Node> rightPtr) : smKey_{key}, left_{leftPtr},
+           mid_{midPtr}, right_{rightPtr} {}
+
+Node::~Node() = default;
+
+bool Node::isLeaf() const {
+    if(left_ == nullptr && mid_ == nullptr && right_ == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+bool Node::isTwoNode() const {
+    if(right_ == nullptr) {
+        return true;
+    }
+    return false;
+}
+
+bool Node::isThreeNode() const {
+    if(right_ != nullptr) {
+        return true;
+    }
+    return false;
+}
+
+std::string Node::getSmKey() const {
+    return smKey_;
+}
+
+std::string Node::getLgKey() const {
+    return lgKey_;
+}
+
+int Node::getSmVal() const {
+    return smVal_;
+}
+
+int Node::getLgVal() const {
+    return lgVal_;
+}
+
+void Node::setSmKey(const std::string &key) {
+    smKey_ = key;
+}
+
+void Node::setLgKey(const std::string &key) {
+    lgKey_ = key;
+}
+
+void Node::setSmVal(const int &val) {
+    smVal_ = val;
+}
+
+void Node::setLgVal(const int &val) {
+    lgVal_ = val;
+}
+
+std::shared_ptr<Node> Node::getLeftPtr() const {
+    return left_;
+}
+
+std::shared_ptr<Node> Node::getMidPtr() const {
+    return mid_;
+}
+
+std::shared_ptr<Node> Node::getRightPtr() const {
+    return right_;
+}
+
+void Node::setLeftPtr(std::shared_ptr<Node> leftPtr) {
+    left_ = leftPtr;
+}
+
+void Node::setMidPtr(std::shared_ptr<Node> midPtr) {
+    mid_ = midPtr;
+}
+
+void Node::setRightPtr(std::shared_ptr<Node> rightPtr) {
+    right_ = rightPtr;
+}
+
+
--- Node.h
+++ Node.h
@@ -4,11 +4,45 @@
 
 #ifndef HW4_NODE_H
 #define HW4_NODE_H
-
+#include <string>
+#include <memory>
 
 class Node {
+private:
+    std::string smKey_, lgKey_;
+    int smVal_, lgVal_;
+    std::shared_ptr<Node> left_;
+    std::shared_ptr<Node> mid_;
+    std::shared_ptr<Node> right_;
 
+public:
+    Node();
+    explicit Node(const std::string& key);
+    Node(const std::string& key, std::shared_ptr<Node> leftPtr,
+         std::shared_ptr<Node> midPtr, std::shared_ptr<Node> rightPtr);
+    ~Node();
+
+    bool isLeaf() const;
+    bool isTwoNode() const;
+    bool isThreeNode() const;
+
+    std::string getSmKey() const;
+    std::string getLgKey() const;
+    int getSmVal() const;
+    int getLgVal() const;
+
+    void setSmKey(const std::string &key);
+    void setLgKey(const std::string &key);
+    void setSmVal(const int &val);
+    void setLgVal(const int &val);
+
+    std::shared_ptr<Node> getLeftPtr() const;
+    std::shared_ptr<Node> getMidPtr() const;
+    std::shared_ptr<Node> getRightPtr() const;
+
+    void setLeftPtr(std::shared_ptr<Node> leftPtr);
+    void setMidPtr(std::shared_ptr<Node> midPtr);
+    void setRightPtr(std::shared_ptr<Node> rightPtr);
 };
-
 
 #endif //HW4_NODE_H
